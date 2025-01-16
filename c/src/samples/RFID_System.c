006c8f7cd20d4aee2adfab7e27e4da2a38f75976
covjus01 <covjus01@gmail.com>
2025-01-15 19:18:36 -0500

    Added Mercury API for C

---
+++ c/src/samples/RFID_System.c
@@ -0,0 +1,6 @@
+//This file is meant to display the system process of connecting, initializing
+//and reading RFID tags using the ThinkMagic Reader. Using the MercuryAPI
+//we will be able to complete these things.
+
+
+
